c12ce42093382e0860a23ed448780cb876624554
ecolladay <ecolladay@gmail.com>
2020-01-15 19:26:03 -0500

    fixed errors

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-function multiplyBy()
-{
+function multiplyBy(){
         num1 = document.getElementById("firstNumber").value;
-        num2 = document.getElementById("secondNumber").value;
-        document.getElementById("result").innerHTML = num1 * num2;
+num2 = document.getElementById("secondNumber").value;
+document.getElementById("result").innerHTML = num1 * num2;
 }
 
-function divideBy() 
-{ 
-        num1 = document.getElementById("firstNumber").value;
-        num2 = document.getElementById("secondNumber").value;
+function divideBy(){ 
+num1 = document.getElementById("firstNumber").value;
+num2 = document.getElementById("secondNumber").value;
 document.getElementById("result").innerHTML = num1 / num2;
 }
